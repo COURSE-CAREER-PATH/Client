4b224052d5940900f59e6a0d4b3203cf0d1e0e79
Prime-02 <163609502+Prime-02@users.noreply.github.com>
2024-10-30 01:26:29 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        Ubuntu: ["Ubuntu"],
-        playwrite: ['"Playwrite DK Uloopet"', "sans-serif"],
-        josefin: ["Josefin Sans", "sans-serif"],
-        sans: ["Inter", "sans-serif"],
-      },
+      fontFamily:{
+        Ubuntu: ['Ubuntu'],
+        'playwrite': ['"Playwrite DK Uloopet"', 'sans-serif'],
+        'josefin': ["Josefin Sans", "sans-serif"]
+      
+      }
     },
   },
-  plugins: [require("tailwind-scrollbar")],
-};
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
